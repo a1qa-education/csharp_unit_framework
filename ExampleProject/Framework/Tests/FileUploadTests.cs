96598cef7d6f737febf40b689a5ca91a9ea10f93
Denis Drozdov <d.drozdov@a1qa.com>
2026-09-08 10:56:09 +0300

    remove unused

--- ExampleProject/Framework/Tests/FileUploadTests.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ExampleProject.Framework.Tests
-{
-    internal class FileUploadTests : BaseTest
-    {
-        private static readonly string fileName = testdata.GetValue<string>("fileUpload.fileName");
-        private static readonly string filePath = testdata.GetValue<string>("fileUpload.filePath") + fileName;
-    }
-}
